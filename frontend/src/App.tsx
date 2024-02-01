f23ef824c9ec9ae11d6493c581f84f6b34c1a0e0
YanBastos <yanmoraesbastos@hotmail.com>
2024-02-01 11:58:29 -0300

    ...

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
-export default function App(){
-  return(
+export  default function App(){
+  return (
     <div>
-      <h1>Meu App React</h1>
+      <h1 className="text-3xl font-bold text-green-400">Meu App React</h1>
+
     </div>
   )
 }
