b8938dc24898447a579761ec9f3e8e484f219f4c
YanBastos <yanmoraesbastos@hotmail.com>
2024-02-02 23:08:29 -0300

    ...

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 export  default function App(){
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-green-400">Meu App React</h1>
-
+    <div className="w-full min-h-screen bg-gray-700 flex justify-center px-4">
+      <main className="my-10 w-full bg-red-500 md:max-w-2xl md:bg-green-500">
+        <h1 className="text-4xl font-medium text-white m-2">Clientes</h1>
+        <p>Texto exemplo para commit</p>
+      </main>
     </div>
   )
 }
